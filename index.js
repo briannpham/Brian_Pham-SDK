c5e77694f54913e6c7445a8c2e2cf919edb62114
Brian Pham <br.pham13@gmail.com>
2022-12-12 21:13:33 -0800

    finished implementing SDKs for all routes

--- index.js
+++ index.js
@@ -4,6 +4,13 @@ const fetch = require("node-fetch");
 const baseURL = "https://the-one-api.dev/v2";
 const ACCESS_KEY = '8-0OVb4azr85RKHwQwvG';
 
+const config = {
+  headers: {
+    'Content-Type': 'application/json',
+    'Authorization': `Bearer ${ACCESS_KEY}`
+  }
+}
+
 /**
  * Get all "The Lord of The Rings" books
  * @returns {Array} List of all books
@@ -18,11 +25,11 @@ const getAllBooks = async () => {
 
 /**
  * Get one specific book
- * @param {string} id - Book id
- * @returns {Object} A book with id equals params id
+ * @param {string} bookId - Book id
+ * @returns {Object} A book with id equals param id
  */
-const getBookById = async (id) => {
-  const response = await fetch(`${baseURL}/book/${id}`);
+const getBookById = async (bookId) => {
+  const response = await fetch(`${baseURL}/book/${bookId}`);
   const data = await response.json();
 
   return data.docs[0];
@@ -31,11 +38,11 @@ const getBookById = async (id) => {
 
 /**
  * Get all chapters of a specific book
- * @param {string} - id - Book id
- * @returns {Array} - An array of all chapters 
+ * @param {string} bookId - Book id
+ * @returns {Array} An array of all chapters 
  */
-const getAllChapters = async (id) => {
-  const response = await fetch(`${baseURL}/book/${id}/chapter`)
+const getAllChaptersOfABook = async (bookId) => {
+  const response = await fetch(`${baseURL}/book/${bookId}/chapter`)
   const data = await response.json()
 
   return data.docs
@@ -47,22 +54,124 @@ const getAllChapters = async (id) => {
  * @returns {Array} List of all movies
  */
 const getAllMovies = async () => {
-  const response = await fetch(`${baseURL}/movie`, {
-    headers: {
-      'Content-Type': 'application/json',
-      'Authorization': `Bearer ${ACCESS_KEY}`
-    }
-  })
+  const response = await fetch(`${baseURL}/movie`, config)
   const data = await response.json()
 
   return data.docs
 }
 
 
+/**
+ * Get a movie by id
+ * @param {string} movieId - Movie id
+ * @return {Object} A movie with id equals param id
+ */
+const getMovieById = async (movieId) => {
+  const response = await fetch(`${baseURL}/movie/${movieId}`, config);
+  const data  = await response.json();
+
+  return data.docs[0]
+}
+
+
+/**
+ * Get all characters
+ * @return {Array} List of all characters
+ */
+const getCharacters = async () => {
+  const response = await fetch(`${baseURL}/character`, config)
+  const data = await response.json()
+
+  return data.docs
+}
+
+
+/**
+ * Get a character by id
+ * @param {string} characterId - Character id
+ * @return {Object} A character with id equals param id
+ */
+const getCharacterById = async (characterId) => {
+  const response = await fetch(`${baseURL}/character/${characterId}`, config)
+  const data = await response.json()
+
+  return data.docs[0]
+}
+
+
+/**
+ * Get all movie quotes of a specific character
+ * @param {string} characterId - Character id
+ * @return {Array} List of all quotes of a character
+ */
+const getAllQuotesOfACharacter = async (characterId) => {
+  const response = await fetch(`${baseURL}/character/${characterId}/quote`, config)
+  const data = await response.json()
+
+  return data.docs
+}
+
+
+/**
+ * Get all movie quotes
+ * @return {Array} List of all movie quotes
+ */
+const getAllQuotes = async () => {
+  const response = await fetch(`${baseURL}/quote`, config)
+  const data = await response.json()
+
+  return data.docs
+}
+
+
+/**
+ * Get a specific movie quote
+ * @param {string} quoteId - Quote id
+ * @return {Object} A quote with a given id
+ */
+const getQuoteById = async (quoteId) => {
+  const response = await fetch(`${baseURL}/quote/${quoteId}`, config)
+  const data = await response.json()
+
+  return data.docs[0]
+}
+
+
+/**
+ * Get all book chapters
+ * @return {Array} List of all book chapters
+ */
+const getAllChapters = async () => {
+  const response = await fetch(`${baseURL}/chapter`, config)
+  const data = await response.json()
+
+  return data.docs
+}
+
+
+/**
+ * Get one specific book chapter
+ * @param {string} chapterId - Chapter id
+ * @return {Object} A book chapter with a given id
+ */
+const getChapterById = async (chapterId) => {
+  const response = await fetch(`${baseURL}/chapter/${chapterId}`, config)
+  const data = await response.json()
+
+  return data.docs[0]
+}
 
 module.exports = {
   getAllBooks,
   getBookById,
+  getAllChaptersOfABook,
+  getAllMovies,
+  getMovieById,
+  getCharacters,
+  getCharacterById,
+  getAllQuotesOfACharacter,
+  getAllQuotes,
+  getQuoteById,
   getAllChapters,
-  getAllMovies
+  getChapterById
 }
--- index.test.js
+++ index.test.js
@@ -1,9 +1,18 @@
 const { 
     getAllBooks, 
     getBookById,
+    getAllChaptersOfABook,
+    getAllMovies,
+    getMovieById,
+    getCharacters,
+    getCharacterById,
+    getAllQuotesOfACharacter,
+    getAllQuotes,
+    getQuoteById,
     getAllChapters,
-    getAllMovies
+    getChapterById
 } = require('./index')
+
 
 it('Get all books', async () => {
     const firstBook = {
@@ -35,7 +44,7 @@ it('Get a book with id', async () => {
 })
 
 it('Get all chapters of a book', async () => {
-    const chapters = await getAllChapters('5cf5805fb53e011a64671582')
+    const chapters = await getAllChaptersOfABook('5cf5805fb53e011a64671582')
 
     expect(chapters[0]).toHaveProperty('chapterName')
     expect(chapters.length).toBeGreaterThan(3)
@@ -48,4 +57,77 @@ it('Get all LOTR movies', async () => {
     expect(movies[0]).toHaveProperty('boxOfficeRevenueInMillions')
     expect(movies[0]).toHaveProperty('rottenTomatoesScore')
     expect(movies[0]['_id']).toEqual('5cd95395de30eff6ebccde56')
+})
+
+it('Get a movie with id', async () => {
+    const movie = await getMovieById('5cd95395de30eff6ebccde56');
+
+    expect(movie).toHaveProperty('boxOfficeRevenueInMillions');
+    expect(movie.boxOfficeRevenueInMillions).toEqual(2917)
+    expect(movie.rottenTomatoesScore).toEqual(94);
+})
+
+it('Get all characters', async () => {
+    const characters = await getCharacters();
+
+    expect(characters.length).toBeGreaterThan(0);
+    expect(characters[0]).toHaveProperty('race');
+    expect(characters[0]).toHaveProperty('realm');
+    expect(characters[0].race).toEqual('Human')
+    expect(characters[0].gender).toEqual('Female')
+    expect(characters[0].realm).toEqual('')
+})
+
+it('Get a character with id', async () => {
+    const character = await getCharacterById('5cd99d4bde30eff6ebccfc1f');
+
+    expect(character.race).toEqual('Human');
+    expect(character.name).toEqual('Bain');
+    expect(character.death).toEqual('TA 3007');
+})
+
+it('Get all movie quotes of a character', async () => {
+    const allQuotes = await getAllQuotesOfACharacter('5cd99d4bde30eff6ebccfc1f');
+
+    expect(allQuotes.length).toEqual(0);
+})
+
+it('Get all movie quotes', async () => {
+    const allQuotes = await getAllQuotes();
+
+    expect(allQuotes.length).toBeGreaterThan(0);
+    expect(allQuotes[0].dialog).toEqual('Deagol!')
+    expect(allQuotes[3].dialog).toEqual('Give us that! Deagol my love')
+})
+
+it('Get a specific movie quote with id', async () => {
+    const quote = await getQuoteById('5cd96e05de30eff6ebcce848')
+
+    expect(quote.dialog).toEqual('Free? He will never be free!')
+    expect(quote.movie).toEqual('5cd95395de30eff6ebccde5d')
+})
+
+it('Get all book chapters', async () => {
+    const chapters = await getAllChapters();
+
+    expect(chapters.length).toBeGreaterThan(0)
+    expect(chapters[0]).toHaveProperty('chapterName')
+})
+
+it('Get a specific book chapter with id', async () => {
+    const chapter = await getChapterById('6091b6d6d58360f988133bc4');
+    const book = await getBookById(chapter.book)
+
+    expect(chapter.chapterName).toEqual('The Steward and the King');
+    expect(book.name).toEqual('The Return Of The King')
+
+})
+
+it('Return a correct character name from a quote id', async () => {
+    const quote = await getQuoteById('5cd96e05de30eff6ebcce849');
+    const character = await getCharacterById(quote.character)
+
+    expect(quote.dialog).toEqual('No!')
+    expect(character.name).toEqual('Gríma Wormtongue')
+
 })
